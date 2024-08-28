c0e8c3485bef8384767d4d4f4a6d75938498e013
looSeeCan <lly@icstars.org>
2024-08-28 14:25:48 -0500

    .....

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const anotherPage = require("./routes/another_page");
-console.log("////////////////////////////////////////////", anotherPage);
 app.use("/", anotherPage); /// "/" mount to localhost3000. so basically: localhost3000/another_page
 
 app.use(express.static(path.join(__dirname, "public")));
@@ -19,3 +18,5 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is listening on http://localhost:${PORT}`);
 });
+
+//TODO: continue attempting Heroku. and/or ngrok
